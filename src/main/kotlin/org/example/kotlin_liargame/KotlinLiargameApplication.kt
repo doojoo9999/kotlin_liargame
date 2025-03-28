4aa3b56b2a3553c7886ce0028fc9947a4e91eb4b
doojoo9999 <parkbr95@naver.com>
2025-03-29 05:50:23 +0900

    feat : 초기 게임 시스템 개발 - 게임 생성 - 라이어 투표 - 정답 맞추기

---
+++ src/main/kotlin/org/example/kotlin_liargame/KotlinLiargameApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlin_liargame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinLiargameApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinLiargameApplication>(*args)
+}
