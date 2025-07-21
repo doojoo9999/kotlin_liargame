2b2b8dd60d4599934d763e97d48b3d210da69c65
doojoo9999 <parkbr95@naver.com>
2025-07-21 15:38:42 +0900

    feat) swagger 추가

--- src/main/kotlin/org/example/kotlin_liargame/domain/question/dto/ApplyQuestionRequest.kt
+++ src/main/kotlin/org/example/kotlin_liargame/domain/question/dto/ApplyQuestionRequest.kt
@@ -1,13 +1,13 @@
 package org.example.kotlin_liargame.domain.question.dto
 
 import org.example.kotlin_liargame.domain.question.model.QuestionEntity
+import javax.security.auth.Subject
 
 data class ApplyQuestionRequest(
     val subject: String,
     val question: String
 ) {
     fun to() = QuestionEntity(
-        subject = this.subject,
         question = this.question
     )
 }
