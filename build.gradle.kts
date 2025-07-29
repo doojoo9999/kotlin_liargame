d8344c6e817b7958f2394d6b5f9aa8020b517286
doojoo9999 <parkbr95@naver.com>
2025-07-30 08:51:43 +0900

    feat: dotenv 라이브러리 추가
    - 환경 변수 관리 도구로 `dotenv-java` 라이브러리 적용
    
    Signed-off-by: doojoo9999 <parkbr95@naver.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.github.cdimascio:dotenv-java:2.3.2")
 
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
