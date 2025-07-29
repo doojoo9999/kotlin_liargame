669b37851fd04354bf0c968ea6a56a69f3316266
doojoo9999 <parkbr95@naver.com>
2025-07-30 08:55:48 +0900

    feat: LeaveGameRequest DTO 추가
    - 게임 나가기 요청 데이터 구조 정의
    - `gNumber` 유효성 검사 로직 포함
    
    Signed-off-by: doojoo9999 <parkbr95@naver.com>

---
+++ src/main/kotlin/org/example/kotlin_liargame/domain/game/dto/request/LeaveGameRequest.kt
@@ -0,0 +1,11 @@
+package org.example.kotlin_liargame.domain.game.dto.request
+
+data class LeaveGameRequest(
+    val gNumber: Int
+) {
+    fun validate() {
+        if (gNumber <= 0) {
+            throw IllegalArgumentException("Game number must be positive")
+        }
+    }
+}
