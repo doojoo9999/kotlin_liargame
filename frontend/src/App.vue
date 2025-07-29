f929381ff34ae9da30fc4ff038b04106d08912af
doojoo9999 <parkbr95@naver.com>
2025-07-29 11:06:00 +0900

    refactor) App.vue 초기 템플릿 코드 제거 및 router-view 기반 구조로 변경
    - 불필요한 코드와 컴포넌트 삭제 (`HelloWorld`, `TheWelcome` 등)
    - Vue Router를 활용한 동적 화면 렌더링 구조로 수정
    - 기본 스타일 설정 개선: 전역 폰트 및 웹 브라우저 렌더링 최적화 추가
    
    Signed-off-by: doojoo9999 <parkbr95@naver.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// App.vue is the root component
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" height="125" src="./assets/logo.svg" width="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  font-family: 'Noto Sans KR', 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 </style>
