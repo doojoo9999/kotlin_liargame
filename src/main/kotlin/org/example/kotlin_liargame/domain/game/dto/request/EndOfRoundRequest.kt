9c3f162d77f43080c76b0b6f981a0e4f322d91d9
doojoo9999 <parkbr95@naver.com>
2025-08-05 16:37:08 +0900

    refactor: EndOfRoundRequest에서 변수명 일원화
    - gNumber 등을 gameNumber로 변수명 변경
    - 변수명 일관성을 위한 유지보수 작업

--- src/main/kotlin/org/example/kotlin_liargame/domain/game/dto/request/EndOfRoundRequest.kt
+++ src/main/kotlin/org/example/kotlin_liargame/domain/game/dto/request/EndOfRoundRequest.kt
@@ -1,11 +1,11 @@
 package org.example.kotlin_liargame.domain.game.dto.request
 
 data class EndOfRoundRequest(
-    val gNumber: Int,
-    val gOwner: String,
-    val gRound: Int,
-    val gLiar: String,
-    val gWinner: String,
-    val gIsGameOver: Boolean,
+    val gameNumber: Int,
+    val gameOwner: String,
+    val gameRound: Int,
+    val gameLiar: String,
+    val gameWinner: String,
+    val isGameOver: Boolean,
 )
 
