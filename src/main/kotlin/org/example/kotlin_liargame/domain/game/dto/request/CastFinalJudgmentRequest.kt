751035db5abc56979dffd4990760eb0b57010ebf
doojoo9999 <parkbr95@naver.com>
2025-08-07 13:03:30 +0900

    feat: CastFinalJudgmentRequest 필드 수정
    - judgment 필드를 voteForExecution(Boolean)으로 변경
    - 투표 의도를 명확히 하기 위해 필드명과 타입 수정
    - 유효성 검사 로직 수정

--- src/main/kotlin/org/example/kotlin_liargame/domain/game/dto/request/CastFinalJudgmentRequest.kt
+++ src/main/kotlin/org/example/kotlin_liargame/domain/game/dto/request/CastFinalJudgmentRequest.kt
@@ -1,10 +1,8 @@
 package org.example.kotlin_liargame.domain.game.dto.request
 
-import org.example.kotlin_liargame.domain.game.model.enum.FinalJudgment
-
 data class CastFinalJudgmentRequest(
     val gameNumber: Int,
-    val judgment: FinalJudgment
+    val voteForExecution: Boolean // true = 처형, false = 생존
 ) {
     fun validate() {
         require(gameNumber > 0) { "Game number must be positive" }
