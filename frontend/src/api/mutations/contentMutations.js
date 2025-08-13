77dcb9472b50711b3015d0bf4f8c79408d96faca
doojoo9999 <parkbr95@naver.com>
2025-08-13 15:55:31 +0900

    refactor: API 함수 매개변수 명확화 및 주석 추가
    - `addSubject` 및 `addWord` 함수의 매개변수 명칭을 일관성 있게 수정
    - 각 함수에 JSDoc 주석 추가로 코드 가독성과 유지보수성 향상

--- frontend/src/api/mutations/contentMutations.js
+++ frontend/src/api/mutations/contentMutations.js
@@ -1,11 +1,20 @@
 import apiClient from '../apiClient';
 
-export const addSubject = async (name) => {
-  const response = await apiClient.post('/subjects/applysubj', { name });
-  return response;
+/**
+ * Adds a new subject.
+ * @param {string} subjectName - The name of the new subject.
+ * @returns {Promise<any>}
+ */
+export const addSubject = async (subjectName) => {
+  return await apiClient.post('/subjects/applysubj', { subject: subjectName });
 };
 
-export const addWord = async (subject, word) => {
-  const response = await apiClient.post('/words/applyw', { subject, word });
-  return response;
+/**
+ * Adds a new word to a subject.
+ * @param {string} subjectName - The name of the subject.
+ * @param {string} word - The new word to add.
+ * @returns {Promise<any>}
+ */
+export const addWord = async (subjectName, word) => {
+  return await apiClient.post('/words/applyw', { subject: subjectName, word });
 };
