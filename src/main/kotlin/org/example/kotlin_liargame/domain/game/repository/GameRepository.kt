da346db7572fa433ee2ab3fce7c05a6e8f27c1aa
doojoo9999 <parkbr95@naver.com>
2025-08-20 23:16:35 +0900

    feat: GameRepository에 비관적 잠금 적용 메서드 추가

--- src/main/kotlin/org/example/kotlin_liargame/domain/game/repository/GameRepository.kt
+++ src/main/kotlin/org/example/kotlin_liargame/domain/game/repository/GameRepository.kt
@@ -1,15 +1,22 @@
 package org.example.kotlin_liargame.domain.game.repository
 
+import jakarta.persistence.LockModeType
 import org.example.kotlin_liargame.domain.game.model.GameEntity
 import org.example.kotlin_liargame.domain.game.model.enum.GameState
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
 
 interface GameRepository : JpaRepository<GameEntity, Long> {
 
     fun findByGameOwner(gameOwner: String) : GameEntity?
+    
     @Query("SELECT g FROM GameEntity g WHERE g.gameNumber = :gameNumber")
     fun findByGameNumber(gameNumber: Int) : GameEntity?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT g FROM GameEntity g WHERE g.gameNumber = :gameNumber")
+    fun findByGameNumberWithLock(gameNumber: Int): GameEntity?
 
     @Query("SELECT g FROM GameEntity g WHERE g.gameState != :gameState")
     fun findByGameStateNot(gameState: GameState): List<GameEntity>
