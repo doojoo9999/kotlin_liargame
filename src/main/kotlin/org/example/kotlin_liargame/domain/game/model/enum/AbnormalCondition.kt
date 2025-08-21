6910e9404d6a176bbf19fd5f5ccee9889dd9da8b
doojoo9999 <parkbr95@naver.com>
2025-08-21 13:13:24 +0900

    AbnormalCondition enum 클래스 추가
    - ALL_PLAYERS_DISCONNECTED, GAME_STUCK, SERVER_ERROR, TIMEOUT_EXCEEDED 상태 정의

---
+++ src/main/kotlin/org/example/kotlin_liargame/domain/game/model/enum/AbnormalCondition.kt
@@ -0,0 +1,8 @@
+package org.example.kotlin_liargame.domain.game.model.enum
+
+enum class AbnormalCondition {
+    ALL_PLAYERS_DISCONNECTED,
+    GAME_STUCK,
+    SERVER_ERROR,
+    TIMEOUT_EXCEEDED
+}
